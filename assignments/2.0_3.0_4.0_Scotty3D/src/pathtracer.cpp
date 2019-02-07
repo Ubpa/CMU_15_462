dfe8868b64a622ad57f47a5f2ae27c9ba4f6d9be
Ubpa <641614112@qq.com>
2019-02-07 17:35:48 +0800

    use cos to terminate

--- assignments/2.0_3.0_4.0_Scotty3D/src/pathtracer.cpp
+++ assignments/2.0_3.0_4.0_Scotty3D/src/pathtracer.cpp
@@ -511,7 +511,9 @@ Spectrum PathTracer::trace_ray(const Ray &r) {
 	
 	// (2) potentially terminate path (using Russian roulette)
 	float terminateProbability = 0.f;
-	if (!isect.bsdf->is_delta() && matF.illum() < 0.2f)
+	// Pareto principle : 2-8 principle
+	// 0.2 * cos(PI / 2 * 0.8) == 0.0618
+	if (!isect.bsdf->is_delta() && matF.illum() * abs(mat_w_in.z) < 0.0618f)
 		terminateProbability = 0.8f;
 
 	if (rand() / float(RAND_MAX) < terminateProbability)
